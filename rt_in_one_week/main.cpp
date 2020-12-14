d26e09280486411352a65a1c5760a88268197221
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-14 09:05:12 -0500

    fix --j

--- rt_in_one_week/main.cpp
+++ rt_in_one_week/main.cpp
@@ -10,7 +10,7 @@ int main()
     std::cout << "P3\n"
               << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; ++j)
+    for (int j = image_height - 1; j >= 0; --j)
     {
         for (int i = 0; i < image_width; ++i)
         {
