b372eab5e555fc7313a8b5b1c9287cb1d700493b
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-15 14:20:38 -0500

    fuzzed metal

--- rt_in_one_weekend/material.h
+++ rt_in_one_weekend/material.h
@@ -36,18 +36,21 @@ public:
 
 class metal : public material {
 public:
-  metal(const color &a) : albedo(a) {}
+  metal(const color &a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
   virtual bool scatter(const ray &r_in, const hit_record &rec,
                        color &attenuation, ray &scattered) const override {
     vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
     // new ray. start from intersection point, pointing to refelcted direction
-    scattered = ray(rec.p, reflected);
+    // scattered = ray(rec.p, reflected);
+    // add fuzz
+    scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
     attenuation = albedo;
     return (dot(scattered.direction(), rec.normal) > 0);
   }
 
 public:
   color albedo;
+  double fuzz;
 };
 
 #endif
