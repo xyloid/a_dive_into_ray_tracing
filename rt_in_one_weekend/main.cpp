9aa914459e9f59b3b15be056fb6c463bac0fbee1
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-15 16:14:07 -0500

    glass sphere that sometimes refracts

--- rt_in_one_weekend/main.cpp
+++ rt_in_one_weekend/main.cpp
@@ -94,7 +94,8 @@ int main() {
   auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
   // auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
   // auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-  auto material_center = make_shared<dielectric>(1.5);
+  // auto material_center = make_shared<dielectric>(1.8);
+  auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
   auto material_left = make_shared<dielectric>(1.5);
   auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
--- rt_in_one_weekend/material.h
+++ rt_in_one_weekend/material.h
@@ -59,13 +59,27 @@ public:
 
   virtual bool scatter(const ray &r_in, const hit_record &rec,
                        color &attenuation, ray &scattered) const override {
+    // glass surface absorb nothing, so attenuation is 1,1,1
     attenuation = color(1.0, 1.0, 1.0);
     // assuming one side is always air, air ir = 1.0
     double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
 
     vec3 unit_direction = unit_vector(r_in.direction());
-    vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
-    scattered = ray(rec.p, refracted);
+
+    double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+    double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+    bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+    vec3 direction;
+
+    if (cannot_refract) {
+      direction = reflect(unit_direction, rec.normal);
+
+    } else {
+      direction = refract(unit_direction, rec.normal, refraction_ratio);
+    }
+
+    scattered = ray(rec.p, direction);
     return true;
   }
 
