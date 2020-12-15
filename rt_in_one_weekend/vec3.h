ac1dfcec01b662208cee197fbb261df5f528ccfb
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-15 12:51:37 -0500

    alternative diffuse

--- rt_in_one_weekend/vec3.h
+++ rt_in_one_weekend/vec3.h
@@ -103,8 +103,15 @@ vec3 random_in_unit_sphere() {
   }
 }
 
-vec3 random_unit_vector(){
-    return unit_vector(random_in_unit_sphere());
+vec3 random_unit_vector() { return unit_vector(random_in_unit_sphere()); }
+
+// for alternive diffuse approach
+vec3 random_in_hemisphere(const vec3 &normal) {
+  vec3 in_unit_sphere = random_in_unit_sphere();
+  if (dot(in_unit_sphere, normal) > 0.0) {
+    return in_unit_sphere;
+  }
+  return -in_unit_sphere;
 }
 
 #endif
