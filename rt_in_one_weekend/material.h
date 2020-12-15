989deefb03c16b69b9cb12f6973c229fcc82797e
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-15 14:11:08 -0500

    metal first render

--- rt_in_one_weekend/material.h
+++ rt_in_one_weekend/material.h
@@ -34,4 +34,20 @@ public:
   color albedo;
 };
 
+class metal : public material {
+public:
+  metal(const color &a) : albedo(a) {}
+  virtual bool scatter(const ray &r_in, const hit_record &rec,
+                       color &attenuation, ray &scattered) const override {
+    vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+    // new ray. start from intersection point, pointing to refelcted direction
+    scattered = ray(rec.p, reflected);
+    attenuation = albedo;
+    return (dot(scattered.direction(), rec.normal) > 0);
+  }
+
+public:
+  color albedo;
+};
+
 #endif
--- rt_in_one_weekend/vec3.h
+++ rt_in_one_weekend/vec3.h
@@ -111,4 +111,6 @@ vec3 random_in_unit_sphere() {
 
 vec3 random_unit_vector() { return unit_vector(random_in_unit_sphere()); }
 
+vec3 reflect(const vec3 &v, const vec3 &n) { return v - 2 * dot(v, n) * n; }
+
 #endif
