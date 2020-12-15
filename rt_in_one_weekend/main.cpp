f18c545dbc240bb88bb5e34ab02f588a59e83ed5
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-15 12:28:43 -0500

    first matte object

--- rt_in_one_weekend/main.cpp
+++ rt_in_one_weekend/main.cpp
@@ -48,13 +48,18 @@ color ray_color(const ray &r) {
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-color ray_color(const ray &r, const hittable &world) {
+color ray_color(const ray &r, const hittable &world, int depth) {
+  if (depth <= 0) {
+    return color(0, 0, 0);
+  }
+
   hit_record rec;
+
   if (world.hit(r, 0, infinity, rec)) {
     // return 0.5 * (rec.normal + color(1, 1, 1));
     point3 target = rec.p + rec.normal + random_in_unit_sphere();
     // ray goes into next direction. when will it stop the recursive call?
-    return 0.5 * ray_color(ray(rec.p, target - rec.p), world);
+    return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
   }
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -67,6 +72,7 @@ int main() {
   const int image_width = 800;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
   const int samples_per_pixel = 100;
+  const int max_depth = 50;
 
   // World
   hittable_list world;
@@ -100,7 +106,7 @@ int main() {
         auto u = (i + random_double()) / (image_width - 1);
         auto v = (j + random_double()) / (image_height - 1);
         ray r = cam.get_ray(u, v);
-        pixel_color += ray_color(r, world);
+        pixel_color += ray_color(r, world, max_depth);
       }
       write_color(std::cout, pixel_color, samples_per_pixel);
 
