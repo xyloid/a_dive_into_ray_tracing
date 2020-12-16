3118eafb846d69d8ef5337bc7fa9ad07cb069d2b
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-15 21:07:15 -0500

    add defocus blur

--- rt_in_one_weekend/camera.h
+++ rt_in_one_weekend/camera.h
@@ -7,7 +7,7 @@ class camera {
 public:
   camera(point3 lookfrom, point3 lookat, vec3 vup,
          double vfov, // vertical field-of-view in degrees
-         double aspect_ratio) {
+         double aspect_ratio, double aperture, double focus_dist) {
     // auto aspect_ratio = 16.0 / 9.0;
     auto theta = degree_to_radians(vfov);
     auto h = tan(theta / 2);
@@ -16,9 +16,9 @@ public:
     auto focal_length = 1.0;
 
     // compute coordinate frame
-    auto w = unit_vector(lookfrom - lookat);
-    auto u = unit_vector(cross(vup, w));
-    auto v = cross(w, u);
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
 
     // origin = point3(0, 0, 0);
     // horizontal = vec3(viewport_width, 0, 0);
@@ -26,19 +26,34 @@ public:
     // lower_left_corner =
     //     origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
+    // origin = lookfrom;
+    // horizontal = viewport_width * u;
+    // vertical = viewport_height * v;
+    // lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
+
     origin = lookfrom;
-    horizontal = viewport_width * u;
-    vertical = viewport_height * v;
-    lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
+    horizontal = focus_dist * viewport_width * u;
+    vertical = focus_dist * viewport_height * v;
+    lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
+
+    lens_radius = aperture / 2;
   }
 
   // ray get_ray(double u, double v) const {
   //   return ray(origin, lower_left_corner + u * horizontal + v * vertical);
   // }
 
+  // ray get_ray(double s, double t) const {
+  //   return ray(origin,
+  //              lower_left_corner + s * horizontal + t * vertical - origin);
+  // }
+
   ray get_ray(double s, double t) const {
-    return ray(origin,
-               lower_left_corner + s * horizontal + t * vertical - origin);
+    vec3 rd = lens_radius * random_in_unit_disk();
+    vec3 offset = u * rd.x() + v * rd.y();
+    
+    return ray(origin + offset, lower_left_corner + s * horizontal +
+                                    t * vertical - origin - offset);
   }
 
 public:
@@ -46,6 +61,8 @@ public:
   point3 lower_left_corner;
   vec3 horizontal;
   vec3 vertical;
+  vec3 u, v, w;
+  double lens_radius;
 };
 
 #endif
--- rt_in_one_weekend/main.cpp
+++ rt_in_one_weekend/main.cpp
@@ -106,8 +106,16 @@ int main() {
   world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
   world.add(make_shared<sphere>(point3(1.0, 0, -1.0), 0.5, material_right));
   // Camera
-  camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 35.0,
-             aspect_ratio);
+  point3 lookfrom(3, 3, 2);
+  point3 lookat(0, 0, -1);
+  vec3 vup(0, 1, 0);
+  auto dist_to_focus = (lookfrom - lookat).length();
+  auto aperture = 0.5;
+
+  // camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 35.0,
+  //            aspect_ratio);
+
+  camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
   /**
    auto viewport_height = 2.0;
