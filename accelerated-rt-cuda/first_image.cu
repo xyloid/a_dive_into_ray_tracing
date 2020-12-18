fef49762a0aa49441793eb99ecfd7d2671073f99
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-18 09:42:12 -0500

    generate first image

--- accelerated-rt-cuda/first_image.cu
+++ accelerated-rt-cuda/first_image.cu
@@ -1,15 +1,16 @@
+#include "cuda_utils.h"
 #include <iostream>
 
-#define checkCudaErrors(val) check_cuda((val), #val, __FILE__, __LINE__)
-void check_cuda(cudaError_t result, char const *const func,
-                const char *const file, int const line) {
-  if (result) {
-    std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at "
-              << file << ":" << line << " '" << func << "' \n";
-    cudaDeviceReset();
-    exit(99);
-  }
+__global__ void render(float *fb, int max_x, int max_y) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  int j = threadIdx.y + blockIdx.y * blockDim.y;
+  if((i >= max_x) || (j >= max_y)) return;
+  int pixel_index = j*max_x*3 + i*3;
+  fb[pixel_index + 0] = float(i) / max_x;
+  fb[pixel_index + 1] = float(j) / max_y;
+  fb[pixel_index + 2] = 0.2;
 }
+
 
 int main(void) {
   // Image
@@ -19,13 +20,36 @@ int main(void) {
   int num_pixels = nx * ny;
   int fb_size = 3 * num_pixels * sizeof(float);
 
-  // allocate FB
+  // allocate FB, no initialization in CPU memory here.
   float *fb;
   checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
   // divide the work on GPU into blocks of 8x8 threads
   int tx = 8;
   int ty = 8;
+
+  // Render our buffer
+  dim3 blocks(nx/tx+1,ny/ty+1);
+  dim3 threads(tx,ty);
+  render<<<blocks, threads>>>(fb, nx, ny);
+  checkCudaErrors(cudaGetLastError());
+  checkCudaErrors(cudaDeviceSynchronize());
+
+  // Output FB as Image
+  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+  for (int j = ny-1; j >= 0; j--) {
+      for (int i = 0; i < nx; i++) {
+          size_t pixel_index = j*3*nx + i*3;
+          float r = fb[pixel_index + 0];
+          float g = fb[pixel_index + 1];
+          float b = fb[pixel_index + 2];
+          int ir = int(255.99*r);
+          int ig = int(255.99*g);
+          int ib = int(255.99*b);
+          std::cout << ir << " " << ig << " " << ib << "\n";
+      }
+  }
+  checkCudaErrors(cudaFree(fb));
 
   return 0;
 }
