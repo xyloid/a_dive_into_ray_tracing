8a09d11c3f8b6cb4f24063837541968a5e05449d
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-23 22:49:20 -0500

    add dieletric

--- accelerated-rt-cuda/material.h
+++ accelerated-rt-cuda/material.h
@@ -60,7 +60,7 @@ public:
                                   curandState *local_rand_state) const {
     // glossy surface
     // reflect the light
-    vec3 reflected = reflect(unit_vector(r_in.direction()),rec.normal);
+    vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
 
     // the glossy surface will produce a few lights within certain range. this
     // range is determined by fuzz.
@@ -73,6 +73,66 @@ public:
 public:
   vec3 albedo;
   float fuzz;
+};
+
+__device__ float schlick(float cosine, float ref_idx) {
+  float r0 = (1.0f - ref_idx) / (1.0f + ref_idx);
+  r0 = r0 * r0;
+  return r0 + (1.0f - r0) * pow((1.0f - cosine), 5.0f);
+}
+
+__device__ bool refract(const vec3 &v, const vec3 &n, float ni_over_nt,
+                        vec3 &refracted) {
+  vec3 uv = unit_vector(v);
+  float dt = dot(u, v);
+  float discriminant = 1.0f - ni_over_nt * ni_over_nt * (1 - dt * dt);
+  if (discriminant > 0.0) {
+    refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+class dielectric : public material {
+public:
+  __device__ dielectric(float ri) : ref_idx(ri) {}
+  __deivce__ virtual bool scatter(const ray &r_in, const hit_record &rec,
+                                  vec3 &attenuation, ray &scattered,
+                                  curandState *local_state_state) const {
+    vec3 outward_normal;
+    vec3 reflected = reflect(r_in.direction(), rec.normal);
+    float ni_over_nt;
+    attenuation = vec3(1.0, 1.0, 1.0);
+    vec3 refracted;
+    float reflect_prob;
+    float cosine;
+    // check which side of the surface
+    if (dot(r_in.direction(), rec.normal) > 0.0f) {
+      outward_normal = -rec.normal;
+      ni_over_nt = ref_idx;
+      cosine = dot(r_in.direction(), rec.normal) / r_in.direction().length();
+      cosine = sqrt(1.0f - ref_idx * ref_idx * (1 - cosine * cosine));
+    } else {
+      outward_normal = rec.normal;
+      ni_over_nt = 1.0f / ref_idx;
+      cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+    }
+
+    // random reflect or refract
+    if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
+      reflect_prob = schlick(cosine, ref_idx);
+    else
+      reflect_prob = 1.0f;
+    if (curand_uniform(local_rand_state) < reflect_prob)
+      scattered = ray(rec.p, reflected);
+    else
+      scattered = ray(rec.p, refracted);
+    return true;
+  }
+
+public:
+  float ref_idx;
 };
 
 #endif
