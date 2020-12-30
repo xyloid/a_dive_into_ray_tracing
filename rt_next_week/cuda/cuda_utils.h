d6bf1cf3611a3502c9092c9aab5d55b901501341
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-30 11:48:53 -0500

    add code base from accelerated rt with cuda

---
+++ rt_next_week/cuda/cuda_utils.h
@@ -0,0 +1,15 @@
+#ifndef CUDA_UTILS_H
+#define CUDA_UTILS_H
+#include <iostream>
+
+#define checkCudaErrors(val) check_cuda((val), #val, __FILE__, __LINE__)
+void check_cuda(cudaError_t result, char const *const func,
+                const char *const file, int const line) {
+  if (result) {
+    std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at "
+              << file << ":" << line << " '" << func << "' \n";
+    cudaDeviceReset();
+    exit(99);
+  }
+}
+#endif
