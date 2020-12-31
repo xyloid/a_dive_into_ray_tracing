0b652254e7068480d77c6777088f6f641d6b204e
Yilin Xu <xu.yilin@northeastern.edu>
2020-12-31 00:09:19 -0500

    add bounding_box for hittable_list

--- rt_next_week/cuda/hittable_list.h
+++ rt_next_week/cuda/hittable_list.h
@@ -1,6 +1,7 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
 
+#include "aabb.h"
 #include "hittable.h"
 
 class hittable_list : public hittable {
@@ -13,6 +14,9 @@ public:
 
   __device__ virtual bool hit(const ray &r, float t_min, float t_max,
                               hit_record &rec) const;
+
+  __device__ virtual bool bounding_box(float time0, float time1,
+                                       aabb &output_box) const;
 
 public:
   hittable **list;
@@ -34,6 +38,25 @@ __device__ bool hittable_list::hit(const ray &r, float t_min, float t_max,
   }
 
   return hit_anything;
+}
+
+__device__ bool hittable_list::bounding_box(float time0, float time1,
+                                            aabb &output_box) const {
+  if (list_size <= 0)
+    return false;
+  aabb temp_box;
+  bool first_box = true;
+
+  for (int i = 0; i < list_size; i++) {
+    // if next object in the list can not be bounded, return false
+    if (!list[i]->bounding_box(time0, time1, temp_box))
+      return false;
+    // in each iteration, the box could be equal or larger. but it can not be smaller. 
+    output_box = first_box ? temp_box : surrounding_box(output_box, temp_box);
+    first_box = false;
+  }
+
+  return true;
 }
 
 #endif
